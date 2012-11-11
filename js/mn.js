1d39382890ea356392c1c4beefee889459534b3b
Kevin Conley <kevindconley@gmail.com>
2012-11-11 01:25:19 -0800

    basic player state with updates to hangout state

--- js/mn.js
+++ js/mn.js
@@ -1,53 +1,87 @@
-var player;
-
 var HOST = 'http://kevincon.github.com/movienight/';
 
-function showParticipants() {
-	var participants = gapi.hangout.getParticipants();
+var mn_player;
+var mn_state;
 
-	var retVal = '<p>Participants lol: </p><ul>';
-
-	for (var index in participants) {
-		var participant = participants[index];
-
-		if (!participant.person) {
-			retVal += '<li>A participant not running this app</li>';
-		}
-		retVal += '<li>' + participant.person.displayName + '</li>';
-	}
-
-	retVal += '</ul>';
-
-	var div = document.getElementById('participantsDiv');
-
-	div.innerHTML = retVal;
-
-	console.log('got to showParticipants');
+function AppState() {
+	this.playing = false;
+	this.positionFresh = false; // Is position fresh?
+	this.position = 0;
 }
 
+/* Callback for when video player's state changes. */
 function playerStateChanged(newState) {
 	switch (newState) {
 		case 'PLAYING':
 			console.log('Playing');
+			if (!mn_state.playing) {
+				mn_state.playing = true;
+				updateState();
+			}
 			break;
 		case 'PAUSED':
 			console.log('Paused');
+			if (mn_state.playing) {
+				mn_state.playing = false;
+				updateState();
+			}
 			break;
 	}
 }
 
-function playerSeekChanged(position) {
-	console.log('Player seeked to' + position.toString());
+/* Callback for when video player is seeked. */
+function playerSeekChanged(dummy) {
+	var newPosition = mn_player.getPosition();
+	console.log('Seeked to ' + newPosition);
+
+	mn_state.position = newPosition;
+	mn_state.positionFresh = true;
+
+	updateState();
 }
 
+/* Initialize player, register callbacks */
 function playerInit() {
-	player = projekktor('#player', {
+	mn_player = projekktor('#player', {
 						volume: 0.8,
 						playerFlashMP4: HOST + 'jarisplayer.swf',
 						playerFlashMP3: HOST + 'jarisplayer.swf'
 					});
-	player.addListener('state', playerStateChanged);
-	player.addListener('seek', playerSeekChanged);
+	mn_player.addListener('state', playerStateChanged);
+	mn_player.addListener('seek', playerSeekChanged);
+}
+
+/* Force the video player to match the state in sharedState */
+function playerUpdate() {
+	if (mn_state.positionFresh) {
+		mn_player.setPlayhead(mn_state.position);
+		mn_state.positionFresh = false;
+	}
+
+	if (mn_state.playing) {
+		mn_player.setPlay();
+	} else {
+		mn_player.setPause();
+	}
+
+}
+
+/* Extract the AppState from the Google Hangout state */
+function stateUpdated(event) {
+	var rawState = gapi.hangout.data.getValue('AppState');
+	if (rawState == undefined) {
+		console.log('stateUpdated: State received from getValue was undefined.');
+		return;
+	}
+
+	mn_state = jQuery.parseJSON(rawState);
+
+	playerUpdate();
+}
+
+/* Update the Google Hangout state */
+function updateState() {
+	gapi.hangout.data.submitDelta({'AppState': JSON.stringify(mn_state)});
 }
 
 function init() {
@@ -57,6 +91,9 @@ function init() {
 				if (eventObj.isApiReady) {
 					document.getElementById('showParticipants')
 						.style.visibility = 'visible';
+
+					gapi.hangout.data.onStateChanged.add(stateUpdated);
+					stateUpdated();
 				}
 			});
 }
